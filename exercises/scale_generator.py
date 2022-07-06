c10f5552a79c017a86041c9ba99b44d4f5cf76dd
Mark Harrington <mark.harrington@armakuni.com>
2022-07-06 14:03:15 +0100

    Add tests and implementation for solution to scale generator diatonic scale task

--- exercises/scale_generator.py
+++ exercises/scale_generator.py
@@ -8,7 +8,7 @@
 """
 
 
-def get_chromatic_from(starting_note):
+def get_chromatic_sequence(starting_note):
 
     chromatic_sequence = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
@@ -20,3 +20,41 @@ def get_chromatic_from(starting_note):
     complete_sequence = chromatic_sequence[starting_note_index:] + chromatic_sequence[:starting_note_index]
 
     return complete_sequence
+
+
+"""
+    # Get chromatic sequence for starting note
+    # Get length of Chromatic Sequence for Loop around check
+    # Start Diatonic Sequence with Starting Note
+    # Intialise Current Index
+    # Iterate through chromatic sequence for intervals length
+        # If next interval is m, get next note in chromatic sequence
+        # Else if next interval is M get next note + 1 from chromatic sequence
+        # Check and Fix Index for Loop Around Chromatic sequence
+
+    # Return Diatonic Sequence
+"""
+
+def get_diatonic_sequence(starting_note, intervals):
+    
+    chromatic_sequence = get_chromatic_sequence(starting_note)
+
+    chromatic_sequence_length = len(chromatic_sequence)
+    
+    diatonic_sequence = [starting_note]
+
+    current_index = 0
+    
+    for i in intervals:
+        if i == 'm':
+            current_index += 1
+
+        elif i == 'M':
+            current_index += 2
+
+        if current_index >= chromatic_sequence_length:
+            current_index = current_index - chromatic_sequence_length
+            
+        diatonic_sequence.append(chromatic_sequence[current_index])
+
+    return diatonic_sequence
--- exercises/tests/test_scale_generator.py
+++ exercises/tests/test_scale_generator.py
@@ -1,13 +1,24 @@
-from exercises.scale_generator import get_chromatic_from
-
+from exercises.scale_generator import get_chromatic_sequence, get_diatonic_sequence
 
 def test_return_sequence_starting_from_c():
-    assert get_chromatic_from('C') == ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    assert get_chromatic_sequence('C') == ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 
 def test_return_sequence_starting_from_f_sharp():
-    assert get_chromatic_from('F#') == ['F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F']
+    assert get_chromatic_sequence('F#') == ['F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F']
 
 
 def test_return_sequence_starting_from_b_flat():
-    assert get_chromatic_from('Bb') == ['Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A']
+    assert get_chromatic_sequence('Bb') == ['Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A']
+
+
+def test_return_diatonic_sequence_starting_from_c():
+    assert get_diatonic_sequence('C', 'MMmMMMm') == ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
+
+
+def test_return_diatonic_sequence_starting_from_f_sharp():
+    assert get_diatonic_sequence('F#', 'MMmMMMm') == ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'F', 'F#']
+
+
+# def test_return_minor_scale_starting_from_c():
+#     assert get_diatonic_sequence('C', 'MmMMmMM') == ['C', 'D', 'D#', 'F', 'G', 'Ab', 'Bb', 'C']
